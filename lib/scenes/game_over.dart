1b7a140daceac95d5823d84537123ac62d11c90c
Denis Muriungi <55490860+riush03@users.noreply.github.com>
2024-03-11 02:00:12 -0700

    complete project

--- lib/scenes/game_over.dart
+++ lib/scenes/game_over.dart
@@ -25,7 +25,7 @@ class GameOverDialog extends StatelessWidget {
 
   final int score;
   final DitoastGame game;
-  final String jwt = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJkZW5pc2tleXNAYXJjb3JlaGFja3Rob24uaWFtLmdzZXJ2aWNlYWNjb3VudC5jb20iLCJhdWQiOiJnb29nbGUiLCJvcmlnaW5zIjpbImh0dHA6Ly9sb2NhbGhvc3Q6MzAwMCJdLCJ0eXAiOiJzYXZldG93YWxsZXQiLCJwYXlsb2FkIjp7ImdlbmVyaWNPYmplY3RzIjpbeyJpZCI6IjMzODgwMDAwMDAwMjIzMjA4MzUuY29kZWxhYl9vYmplY3QiLCJjbGFzc0lkIjoiMzM4ODAwMDAwMDAyMjMyMDgzNS5jb2RlbGFiX2NsYXNzIiwiZ2VuZXJpY1R5cGUiOiJHRU5FUklDX1RZUEVfVU5TUEVDSUZJRUQiLCJoZXhCYWNrZ3JvdW5kQ29sb3IiOiIjNDI4NWY0IiwibG9nbyI6eyJzb3VyY2VVcmkiOnsidXJpIjoiaHR0cHM6Ly9zdG9yYWdlLmdvb2dsZWFwaXMuY29tL3dhbGxldC1sYWItdG9vbHMtY29kZWxhYi1hcnRpZmFjdHMtcHVibGljL3Bhc3NfZ29vZ2xlX2xvZ28uanBnIn19LCJjYXJkVGl0bGUiOnsiZGVmYXVsdFZhbHVlIjp7Imxhbmd1YWdlIjoiZW4tVVMiLCJ2YWx1ZSI6Ikdvb2dsZSBJL08gJzIyIn19LCJzdWJoZWFkZXIiOnsiZGVmYXVsdFZhbHVlIjp7Imxhbmd1YWdlIjoiZW4tVVMiLCJ2YWx1ZSI6IkF0dGVuZGVlIn19LCJoZWFkZXIiOnsiZGVmYXVsdFZhbHVlIjp7Imxhbmd1YWdlIjoiZW4tVVMiLCJ2YWx1ZSI6IkFsZXggTWNKYWNvYnMifX0sImJhcmNvZGUiOnsidHlwZSI6IlFSX0NPREUiLCJ2YWx1ZSI6IjMzODgwMDAwMDAwMjIzMjA4MzUuY29kZWxhYl9vYmplY3QifSwiaGVyb0ltYWdlIjp7InNvdXJjZVVyaSI6eyJ1cmkiOiJodHRwczovL3N0b3JhZ2UuZ29vZ2xlYXBpcy5jb20vd2FsbGV0LWxhYi10b29scy1jb2RlbGFiLWFydGlmYWN0cy1wdWJsaWMvZ29vZ2xlLWlvLWhlcm8tZGVtby1vbmx5LmpwZyJ9fSwidGV4dE1vZHVsZXNEYXRhIjpbeyJoZWFkZXIiOiJQT0lOVFMiLCJib2R5IjoiMTIzNCIsImlkIjoicG9pbnRzIn0seyJoZWFkZXIiOiJDT05UQUNUUyIsImJvZHkiOiIyMCIsImlkIjoiY29udGFjdHMifV19XX0sImlhdCI6MTcwOTgxNjAwMX0.GlOqq16xffx81VAEXjr_4GarjYCOYFgo8R1Tl-c7LxRT-x-axfXIXISvRCpCIKSbzA7ralpkuYqyFXDGsncaaz_iv2S93Vk16ZZ2fpFycqXVBsCgfDdS2_bix5E1ZAqr15ENdnzsBGEjYRZxKI5Klz2et5j5yHTzeXnaKWX9spVsP3Gjgwxj--XeekktfT_0fRR3UYiC-6r-QBxqlC-iRhu8DAQWY1Z1J6z3c3Nm2MJiG9BR_eEoL-3dvQ1wgp_vqNqDBP-8otBA3suyfxhbblqCIFren4ta7iLEzyk72ftHIfDJKfse25Y9W-V6HzcF4GvDvaMDVyCfOqmM_7vdFA';
+  final String jwt = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJkZW5pc2tleXNAYXJjb3JlaGFja3Rob24uaWFtLmdzZXJ2aWNlYWNjb3VudC5jb20iLCJhdWQiOiJnb29nbGUiLCJvcmlnaW5zIjpbImh0dHA6Ly9sb2NhbGhvc3Q6MzAwMCJdLCJ0eXAiOiJzYXZldG93YWxsZXQiLCJwYXlsb2FkIjp7ImdlbmVyaWNPYmplY3RzIjpbeyJpZCI6IjMzODgwMDAwMDAwMjIzMjA4MzUuZGl0b2FzdF9vYmplY3QiLCJjbGFzc0lkIjoiMzM4ODAwMDAwMDAyMjMyMDgzNS5nYW1lX2NsYXNzIiwiZ2VuZXJpY1R5cGUiOiJHRU5FUklDX1RZUEVfVU5TUEVDSUZJRUQiLCJoZXhCYWNrZ3JvdW5kQ29sb3IiOiIjMjE3MzQwIiwibG9nbyI6eyJzb3VyY2VVcmkiOnsidXJpIjoiaHR0cHM6Ly9kZW5ub3RlY2guY28ua2Uvd3AtY29udGVudC91cGxvYWRzLzIwMjQvMDMvaWNvbnM4LXNhdmUtZWFydGgtNjQucG5nIn19LCJjYXJkVGl0bGUiOnsiZGVmYXVsdFZhbHVlIjp7Imxhbmd1YWdlIjoiZW4tVVMiLCJ2YWx1ZSI6IkdhbWUgUmVkZWVtaW5nIFBhc3MifX0sInN1YmhlYWRlciI6eyJkZWZhdWx0VmFsdWUiOnsibGFuZ3VhZ2UiOiJlbi1VUyIsInZhbHVlIjoiRGl0b2FzdCJ9fSwiaGVhZGVyIjp7ImRlZmF1bHRWYWx1ZSI6eyJsYW5ndWFnZSI6ImVuLVVTIiwidmFsdWUiOiJQYXNzIGZvciByZWRlZW1pbmcgZ2FtZSJ9fSwiYmFyY29kZSI6eyJ0eXBlIjoiUVJfQ09ERSIsInZhbHVlIjoiMzM4ODAwMDAwMDAyMjMyMDgzNS5kaXRvYXN0X29iamVjdCJ9LCJoZXJvSW1hZ2UiOnsic291cmNlVXJpIjp7InVyaSI6Imh0dHBzOi8vZGVubm90ZWNoLmNvLmtlL3dwLWNvbnRlbnQvdXBsb2Fkcy8yMDI0LzAyL1NjcmVlbnNob3RfMS0xNTM2eDcyNy5wbmcifX0sInRleHRNb2R1bGVzRGF0YSI6W3siaGVhZGVyIjoiUE9JTlRTIiwiYm9keSI6IjEyMzQiLCJpZCI6InBvaW50cyJ9LHsiaGVhZGVyIjoiQ09OVEFDVFMiLCJib2R5IjoiMjAiLCJpZCI6ImNvbnRhY3RzIn1dfV19LCJpYXQiOjE3MTAxNDQ2MDd9.Ak6R2AP8xRdVCPpV0FCh-dWxbhchNLOw5dKPxngKvv5L1Cp4S0cYM8LIInolCSvNt8mlEpECS9Hm3d3zKCZMzxeutXMp1YvIB-85GUfCOmLM4av4Z2QjJe3448kaT2kcqmKA6k2zqk2SdZfqtbUh5fBGI3V-Dgp8mAMozQHRms4rRjX9jHxx_WpOU4F4mDNEJwy2EvyouR7dEfaToaE4rlDaZ5ZlBuoIYyI1d8ZWLJDynJsBbgV_e96tzzsFulVOqVjutTE20dcV2HgH-QVmg8Geq0H65IUG8v4l8KbgT0FAdlGNqckcUzEZado5yJ2rYT3WraUKMJXBWIjyYz1OzQ';
 
   @override
   Widget build(BuildContext context) {
@@ -95,10 +95,10 @@ class GameOverDialog extends StatelessWidget {
                          AddToGoogleWebWalletButton(
                              onPressed: () {
                                _savePassBrowser();
+                               context.pop<GameOverAction>(GameOverAction.continueGame);
                              },
                              onSuccess:  () async {
                                 _showSnackBar(context, 'Added to wallet successfully');
-                                context.pop<GameOverAction>(GameOverAction.continueGame);
                              },
                              onCanceled: () => _showSnackBar(context,'Action Cancelled'),
                              onError: (Object error) => _showSnackBar(context, error.toString()),
@@ -184,7 +184,7 @@ enum GameOverAction {
 final String _passId = const Uuid().v4();
 const String _passClass = 'codelab_class';
 const String _issuerId = '3388000000022320835';
-const String _issuerEmail = 'Dennzriush@gmail.com';
+const String _issuerEmail = 'deniskeys@arcorehackthon.iam.gserviceaccount.com';
 
 
 final String _ditoastGamePass = """ 
@@ -199,7 +199,7 @@ final String _ditoastGamePass = """
             "id": "$_issuerId.$_passId",
             "classId": "$_issuerId.$_passClass",
             "genericType": "GENERIC_TYPE_UNSPECIFIED",
-            "hexBackgroundColor": "#4285f4",
+            "hexBackgroundColor": "#217340",
             "logo": {
               "sourceUri": {
                 "uri": "https://storage.googleapis.com/wallet-lab-tools-codelab-artifacts-public/pass_google_logo.jpg"
@@ -208,19 +208,19 @@ final String _ditoastGamePass = """
             "cardTitle": {
               "defaultValue": {
                 "language": "en",
-                "value": "DITOAST GAME '24 [DEMO ONLY]"
+                "value": "Game Redeeming Pass"
               }
             },
             "subheader": {
               "defaultValue": {
                 "language": "en",
-                "value": "Gamer"
+                "value": "Ditoast"
               }
             },
             "header": {
               "defaultValue": {
                 "language": "en",
-                "value": "Player"
+                "value": "Pass for redeeming game"
               }
             },
             "barcode": {
@@ -229,7 +229,7 @@ final String _ditoastGamePass = """
             },
             "heroImage": {
               "sourceUri": {
-                "uri": "https://storage.googleapis.com/wallet-lab-tools-codelab-artifacts-public/google-io-hero-demo-only.jpg"
+                "uri": "https://dennotech.co.ke/wp-content/uploads/2024/02/Screenshot_1-1536x727.png"
               }
             },
             "textModulesData": [
